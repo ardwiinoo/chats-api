8dbea7ca849f99da740ad0f03ee53847bdd0597a
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-08-24 20:25:17 +0700

    feat: add implement login service & jwt claims

--- internal/user/user_service.go
+++ internal/user/user_service.go
@@ -5,11 +5,55 @@ import (
 	"context"
 	"strconv"
 	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+const (
+	secretKey = "chatsapi-xxx"
 )
 
 type service struct {
 	Repository
 	timeout time.Duration
+}
+
+type JWTClaims struct {
+	ID string `json:"id"`
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+// Login implements Service.
+func (s *service) Login(ctx context.Context, req *LoginUserReq) (*LoginUserRes, error) {
+	ctx, cancel := context.WithTimeout(ctx, s.timeout)
+	defer cancel()
+
+	u, err := s.Repository.GetUserByEmail(ctx, req.Email)
+	if err != nil {
+		return &LoginUserRes{}, err
+	}
+
+	err = util.ComparePassword(req.Password, u.Password)
+	if err != nil {
+		return &LoginUserRes{}, err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, JWTClaims{
+		ID: strconv.Itoa(int(u.ID)),
+		Username: u.Username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer: strconv.Itoa(int(u.ID)),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(1 * time.Hour)),
+		},
+	})	
+
+	ss, err := token.SignedString([]byte(secretKey))
+	if err != nil {
+		return &LoginUserRes{}, err
+	}
+
+	return &LoginUserRes{accessToken: ss, Username: u.Username, ID: strconv.Itoa(int(u.ID))}, nil
 }
 
 // CreateUser implements Service.
@@ -24,7 +68,7 @@ func (s *service) CreateUser(ctx context.Context, req *CreateUserReq) (*CreateUs
 
 	u := &User{
 		Username: req.Username,
-		Email: req.Email,
+		Email:    req.Email,
 		Password: hashedPassword,
 	}
 
@@ -34,9 +78,9 @@ func (s *service) CreateUser(ctx context.Context, req *CreateUserReq) (*CreateUs
 	}
 
 	res := &CreateUserRes{
-		ID: strconv.Itoa(int(r.ID)),
+		ID:       strconv.Itoa(int(r.ID)),
 		Username: r.Username,
-		Email: r.Email,
+		Email:    r.Email,
 	}
 
 	return res, nil
